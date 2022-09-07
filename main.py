9ccdade7967c471eb93784cf4e51aef5fe385967
tal <tal@tals-MacBook-Air.local>
2022-09-07 09:02:37 +0300

    reshape stars module

--- main.py
+++ main.py
@@ -9,7 +9,8 @@
 
 # packages  
 import pygame as pygame
-import time as t 
+import numpy  as np 
+import time   as t 
  
 # classes
 from classes.background import Background
@@ -52,6 +53,21 @@ BULLET_MAX    = 2     # magazine size (at most 3 bullets on WINDOW)
 RED = (255,0,0)       # bullet color 
 
 
+
+# STARs
+star_list   = []           # contain all stars present on WINDOW 
+STAR_VEL    = 3            # CHNAGE : to something random 
+STAR_WIDTH  = 40           # star dimensions 
+STAR_HEIGHT = 50   
+STAR_MAX    = 2            # at most 3 stars on WINDOW 
+YELLOW      = (255,255,0)  # stars color 
+
+
+
+
+
+
+
 # EVENTS 
 STAR_HIT  = pygame.USEREVENT + 1 
 STAR_INIT = pygame.USEREVENT + 2  
@@ -61,37 +77,70 @@ STAR_INIT = pygame.USEREVENT + 2
 
 
 
-# DRAW 
+# DRAW BULLETS 
 def draw_bullets(bullet_list):
     for bullet in bullet_list:
         pygame.draw.rect(WINDOW, RED, bullet)
 
 
 # HANDLE BULLETS 
-def handle_bullets(bullet_list, yellow): 
+def handle_bullets(bullet_list, star_list): 
     
     for bullet in bullet_list: 
+        # movement on WINDOW   
+        bullet.y -= BULLET_VEL                  
+        # reached top WINDOW
+        if bullet.y < 0: bullet_list.remove(bullet)    
+
+
+# DRAW STARS          
+def draw_stars(star_list):    
+    
+    for star in star_list:
+        pygame.draw.rect(WINDOW, YELLOW, star)
+
+
+# ADD NEW STARs
+def add_new_stars(star_list):
+    
+    if len(star_list) < STAR_MAX :
+        x = np.random.randint(0, SCREEN_WIDTH - STAR_WIDTH) 
+        star = pygame.Rect(x, 0, STAR_WIDTH, STAR_HEIGHT)       
+        star_list.append(star) 
+
+
+# COLLISION 
+def handle_stars_and_bullets(bullet_list, star_list): 
+    
+    for star in star_list:
+
+        star.y += STAR_VEL
+
+        # star reached buttom 
+        if star.y > SCREEN_HEIGHT: star_list.remove(star)
+            
+        # star & bullet collided 
+        for bullet in bullet_list: 
+            if star.colliderect(bullet): 
+                
+                blast_sound()
+                pygame.event.post(pygame.event.Event(STAR_HIT))         # posting event 
+                bullet_list.remove(bullet)                              # remove bullet (from list) 
+                star_list.remove(star)                                  # remove star   (from list)
+            
+
+
+            
+
         
-        bullet.y -= BULLET_VEL                                      # movement on WINDOW
-        # collision         
-        if yellow.colliderect(bullet): 
-            blast_sound()
-            pygame.event.post(pygame.event.Event(STAR_HIT))         # posting event 
-            bullet_list.remove(bullet)                              # remove bullet 
-        # reached top WINDOW 
-        elif bullet.y < 0:  
-            bullet_list.remove(bullet)
 
-
-
-
-
+        
 
 
 def main():
 
     mouse_clicked = False 
-    star.initialize()
+    #star.initialize()
     stage = 'non'    
     collision_time = t.time() - 1 
     clock = pygame.time.Clock()         # conroll number of looping per second
@@ -103,14 +152,11 @@ def main():
         for event in pygame.event.get():    
             
             # STAR-HIT 
-            if event.type == STAR_HIT:
-                star.status = 'blasted'
-                stage       = '1'
-                collision_time   = t.time() 
-                
+            if event.type == STAR_HIT: print ('star HIT')
+            
             # QUIT-GAME 
-            if event.type == pygame.QUIT: 
-                run = False
+            if event.type == pygame.QUIT: run = False
+                
 
             # BULLET-FIRED 
             if (event.type == pygame.KEYUP):
@@ -126,16 +172,20 @@ def main():
 
         # FIRST LAYER 
         background.draw()
-        
+
+        # STARS
+        add_new_stars(star_list)
+        handle_stars_and_bullets(bullet_list, star_list)
+        draw_stars(star_list)      
+
         # BULLETS
-        handle_bullets(bullet_list, star.rect_star)
+        handle_bullets(bullet_list, star_list)
         draw_bullets(bullet_list)
         
 
         # SPACESHIP
         spaceship.movement()               # moving spaceship on WINDOW 
         spaceship.draw()                   # draw spaceship
-
 
         # FLAME 
         x,y = spaceship.coordinates()
@@ -146,32 +196,32 @@ def main():
         flame.draw()
 
 
-        # STAR      
-        star.update_image()                # for animation 
-        star.draw_star()
-
         
-
-        star_y_coordinate = star.y_coordinate()
-        if (t.time() > collision_time + 0.7) and stage == '1': 
-            stage = '2'
-            star.status = 'not blasted'
-        
-        elif (t.time() > collision_time + 0.7) and stage == '2':
-            stage = 'non'
-            star.initialize()
-
-        elif (star_y_coordinate > SCREEN_HEIGHT) and (star.status == 'not blasted'):
-            
-            pos = pygame.mouse.get_pos()                        # mouse position 
-            if gameOver.new_game(pos,mouse_clicked)  : main()   # NEW-GAME 
-            if gameOver.exit_game(pos,mouse_clicked) : break    # EXIT-GAME 
-            if (pygame.mouse.get_pressed()[0] == 0): mouse_clicked = False
-            gameOver.draw()    
-            
-
-
         pygame.display.update()
+
+
+
+
+        # star_y_coordinate = star.y_coordinate()
+        # if (t.time() > collision_time + 0.7) and stage == '1': 
+        #     stage = '2'
+        #     star.status = 'not blasted'
+        
+        # elif (t.time() > collision_time + 0.7) and stage == '2':
+        #     stage = 'non'
+        #     star.initialize()
+
+        # elif (star_y_coordinate > SCREEN_HEIGHT) and (star.status == 'not blasted'):
+            
+        #     pos = pygame.mouse.get_pos()                        # mouse position 
+        #     if gameOver.new_game(pos,mouse_clicked)  : main()   # NEW-GAME 
+        #     if gameOver.exit_game(pos,mouse_clicked) : break    # EXIT-GAME 
+        #     if (pygame.mouse.get_pressed()[0] == 0): mouse_clicked = False
+        #     gameOver.draw()    
+            
+
+
+        
             
 
             
